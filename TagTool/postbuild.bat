8704e333c25b984edbd09c8cec0cbadcc36b126a
Hamish Arblaster <hamarb123@gmail.com>
2023-11-04 23:20:48 +1100

    Fix usage of msbuild properties in batch file

--- TagTool/postbuild.bat
+++ TagTool/postbuild.bat
@@ -1,9 +1,14 @@
-cd $(ProjectDir)$(OutDir)
-xcopy "$(ProjectDir)..\Physics" "Physics" /e /i /h /Y /d
-xcopy "$(ProjectDir)..\Tools" "Tools" /e /i /h /Y /d
-xcopy "$(ProjectDir)..\ShaderGenerator\HaloShaderGenerator\bin\x64\$(ConfigurationName)" "Tools" /e /i /h /Y /d
+:: $(ProjectDir) in %1
+:: $(OutDir) in %2
+:: $(PlatformName) in %3
+:: $(ConfigurationName) in %4
 
-IF "$(PlatformName)" == "x64" (
+cd %1%2
+xcopy "%1..\Physics" "Physics" /e /i /h /Y /d
+xcopy "%1..\Tools" "Tools" /e /i /h /Y /d
+xcopy "%1..\ShaderGenerator\HaloShaderGenerator\bin\x64\%4" "Tools" /e /i /h /Y /d
+
+IF "%3" == "x64" (
 del "Tools\meshoptimizer32.dll"
 ren "Tools\meshoptimizer64.dll" meshoptimizer.dll
 del "Tools\fmod.dll"
@@ -18,9 +23,9 @@ del "Tools\fmod64.dll"
 :: for /f "eol=: delims=" %25%25F in ('dir /b^|find "System"') do move /Y "%25%25F" "Tools\scripting"
 :: for /f "eol=: delims=" %25%25F in ('dir /b^|find "Microsoft.CodeAnalysis"') do move /Y "%25%25F" "Tools\scripting"
 
-cd $(ProjectDir)
+cd %1
 if ERRORLEVEL == 0 (
-xcopy "$(ProjectDir)..\Tools\HaloShaderGenerator.dll" "$(ProjectDir)$(OutDir)" /e /i /h /Y /d 2&gt;nul 1&gt;nul
+xcopy "%1..\Tools\HaloShaderGenerator.dll" "%1%2" /e /i /h /Y /d 2>nul 1>nul
 SET ERRORLEVEL=0
 )
 EXIT %25ERRORLEVEL%25
