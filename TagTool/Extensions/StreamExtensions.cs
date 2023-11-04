b9b656fbb4d8f53b81ffb2b0412b915e6565aec5
Hamish Arblaster <hamarb123@gmail.com>
2023-11-04 22:20:49 +1100

    Convert tabs to spaces

--- TagTool/Extensions/StreamExtensions.cs
+++ TagTool/Extensions/StreamExtensions.cs
@@ -7,20 +7,20 @@ using System.Threading.Tasks;
 
 namespace TagTool.Extensions
 {
-	public static class StreamExtensions
-	{
-		public static int ReadAll(this Stream s, byte[] buffer, int offset, int count)
-		{
-			int receivedTotal = 0, received;
-			do
-			{
-				received = s.Read(buffer, offset, count);
-				receivedTotal += received;
-				offset += received;
-				count -= received;
-			}
-			while (count > 0 && received > 0);
-			return receivedTotal;
-		}
-	}
+    public static class StreamExtensions
+    {
+        public static int ReadAll(this Stream s, byte[] buffer, int offset, int count)
+        {
+            int receivedTotal = 0, received;
+            do
+            {
+                received = s.Read(buffer, offset, count);
+                receivedTotal += received;
+                offset += received;
+                count -= received;
+            }
+            while (count > 0 && received > 0);
+            return receivedTotal;
+        }
+    }
 }
