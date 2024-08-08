8f8914540922a43bf3016c20aad154ee8141e3ec
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2024-08-08 15:56:51 +1000

    Update Tag Structure Handler

--- TagTool/MtnDewIt/JSON/Handlers/TagStructureHandler.cs
+++ TagTool/MtnDewIt/JSON/Handlers/TagStructureHandler.cs
@@ -3,8 +3,6 @@ using TagTool.Cache;
 using TagTool.Tags;
 using Newtonsoft.Json;
 using System;
-using System.Collections;
-using System.Reflection;
 
 namespace TagTool.MtnDewIt.JSON.Handlers
 {
@@ -22,39 +20,27 @@ namespace TagTool.MtnDewIt.JSON.Handlers
 
         public override void WriteJson(JsonWriter writer, TagStructure value, JsonSerializer serializer) 
         {
-            if (value == null)
-            {
-                writer.WriteNull();
-                return;
-            }
-
-            Type objType = value.GetType();
-
-            if (objType.IsPrimitive || objType == typeof(string) || objType.IsValueType)
-            {
-                writer.WriteValue(value);
-                return;
-            }
-
-            if (typeof(IEnumerable).IsAssignableFrom(objType))
-            {
-                writer.WriteStartArray();
-                foreach (var item in (IEnumerable)value)
-                {
-                    serializer.Serialize(writer, item);
-                }
-                writer.WriteEndArray();
-                return;
-            }
+            var structureInfo = TagStructure.GetTagStructureInfo(value.GetType(), Cache.Version, Cache.Platform);
 
             writer.WriteStartObject();
-            FieldInfo[] fields = objType.GetFields(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var field in fields)
+
+            for (int i = 0; i < TagStructure.GetTagFieldEnumerable(structureInfo).Count; i++)
             {
-                writer.WritePropertyName(field.Name);
-                var fieldValue = field.GetValue(value);
-                serializer.Serialize(writer, fieldValue);
+                var tagFieldInfo = TagStructure.GetTagFieldEnumerable(structureInfo)[i];
+
+                var fieldName = tagFieldInfo.Name;
+                var fieldType = tagFieldInfo.FieldType;
+                var fieldInfo = tagFieldInfo.FieldInfo;
+                var fieldValue = tagFieldInfo.GetValue(value);
+                var isInvalidField = tagFieldInfo.Attribute != null && tagFieldInfo.Attribute.Flags.HasFlag(TagFieldFlags.Padding) || fieldName.Contains("unused", StringComparison.OrdinalIgnoreCase) || fieldName.Contains("padding", StringComparison.OrdinalIgnoreCase);
+
+                if (!isInvalidField)
+                {
+                    writer.WritePropertyName(fieldName);
+                    serializer.Serialize(writer, fieldValue);
+                }
             }
+
             writer.WriteEndObject();
         }
 
