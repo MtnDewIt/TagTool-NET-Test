37b5c01503c9fc13c5368d9ae408489080d1f799
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2024-11-06 15:53:21 +1100

    Fix List Duplicate Tags Command
    It wasn't checking if the tag was null before attempting to pull the tag name and group, so we just use the NonNull() tag table, which has all null entries removed by default

--- TagTool/Commands/Tags/ListDuplicateTagsCommand.cs
+++ TagTool/Commands/Tags/ListDuplicateTagsCommand.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using TagTool.Cache;
-using TagTool.Common;
-using TagTool.Commands.Common;
-using TagTool.Tags;
-using TagTool.Cache.HaloOnline;
 
 namespace TagTool.Commands.Tags
 {
@@ -28,7 +24,7 @@ namespace TagTool.Commands.Tags
 
         public override object Execute(List<string> args)
         {
-            var tags = Cache.TagCacheGenHO.TagTable.GroupBy(x => $"{x.Name}.{x.Group}").Where(x => x.Count() > 1).ToList();
+            var tags = Cache.TagCacheGenHO.NonNull().GroupBy(x => $"{x.Name}.{x.Group}").Where(x => x.Count() > 1).ToList();
             foreach (var duplicates in tags)
             {
                 foreach (var duplicate in duplicates)
