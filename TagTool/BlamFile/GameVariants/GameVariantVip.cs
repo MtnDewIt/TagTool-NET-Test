6c991ff38a22912725b0858c9cd506698efdfd87
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2024-11-17 14:31:49 +1100

    Fix Typo

--- TagTool/BlamFile/GameVariants/GameVariantVip.cs
+++ TagTool/BlamFile/GameVariants/GameVariantVip.cs
@@ -3,7 +3,7 @@ using TagTool.Tags;
 
 namespace TagTool.BlamFile.GameVariants
 {
-    [TagStructure(Size = 0x2, Align = 0x138)]
+    [TagStructure(Size = 0x238, Align = 0x1)]
     public class GameVariantVip : GameVariantBase
     {
         public short ScoreToWinRound;
