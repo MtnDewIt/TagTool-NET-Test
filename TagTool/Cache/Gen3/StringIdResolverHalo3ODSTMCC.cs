72aad7a68e437da72611922ba18762d8bb05236e
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2025-01-15 15:32:25 +1100

    Initial ODST MCC Support
    Data inside the cache will now display correctly including tag groups and string ids

--- TagTool/Cache/Gen3/StringIdResolverHalo3ODSTMCC.cs
+++ TagTool/Cache/Gen3/StringIdResolverHalo3ODSTMCC.cs
@@ -8,15 +8,15 @@ namespace TagTool.Cache
     public class StringIdResolverHalo3ODSTMCC : StringIdResolver
     {
         // These values were figured out through trial-and-error
-        private static readonly int[] SetOffsets = { 0xFBC, 0x55B, 0xBEF, 0xC8A, 0xCE2, 0xDD9, 0xE25, 0xE74, 0xE8C, 0xE99 };
-        private const int SetMin = 0x55A;   // Mininum index that goes in a set
+        private static readonly int[] SetOffsets = { 0xF99, 0x52A, 0xC22, 0xCBD, 0xD15, 0xE66, 0xEB2, 0xF01, 0xF19, 0xF26 };
+        private const int SetMin = 0x52A;   // Mininum index that goes in a set
         private const int SetMax = 0xFFFF; // Maximum index that goes in a set
 
         public StringIdResolverHalo3ODSTMCC()
         {
-            LengthBits = 7;
+            LengthBits = 5;
             SetBits = 8;
-            IndexBits = 17;
+            IndexBits = 19;
         }
 
         public override int GetMinSetStringIndex()
