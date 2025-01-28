67464e210bb6cfa317785487edf03eb49abde024
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2025-01-28 13:25:58 +1100

    Add Reach Health Station Substitution

--- TagTool/Commands/Porting/PortTagCommand.cs
+++ TagTool/Commands/Porting/PortTagCommand.cs
@@ -369,6 +369,7 @@ namespace TagTool.Commands.Porting
                 scenario.AiObjectives.Clear();
                 scenario.AiUserHintData.Clear();
                 scenario.Scripts.Clear();
+                scenario.ScriptSourceFileReferences.Clear();
                 scenario.ScriptStrings = null;
 
                 scenario.CharacterPalette.Clear();
@@ -451,16 +452,6 @@ namespace TagTool.Commands.Porting
 
                 RemoveNullPlacements(scenario.SceneryPalette, scenario.Scenery);
                 RemoveNullPlacements(scenario.CratePalette, scenario.Crates);
-
-                // remove unsupported healthpack controls
-                if (BlamCache.TagCache.TryGetCachedTag("objects\\levels\\shared\\device_controls\\health_station\\health_station.ctrl", out CachedTag healthCtrl))
-                {
-                    short index = (short)scenario.ControlPalette.FindIndex(e => e.Object == healthCtrl);
-                    if (index != -1)
-                        scenario.ControlPalette[index].Object = null;
-
-                    RemoveNullPlacements(scenario.ControlPalette, scenario.Controls);
-                }
             }
 
             //if (definition is SkyAtmParameters skya)
@@ -865,6 +856,10 @@ namespace TagTool.Commands.Porting
 
             switch (blamDefinition)
 			{
+                case AiDialogueGlobals adlg:
+                    ConvertDialogueGlobals(adlg);
+                    break;
+
 				case AreaScreenEffect sefc:
 					if (BlamCache.Version < CacheVersion.Halo3ODST)
 					{
@@ -1218,6 +1213,58 @@ namespace TagTool.Commands.Porting
                             {
                                 block.Flags = block.FlagsReach.ConvertLexical<Scenario.StructureBspBlock.StructureBspFlags>();
                             }
+
+                            // remove unsupported healthpack controls
+                            if (CacheContext.TagCache.TryGetCachedTag("objects\\levels\\shared\\device_controls\\health_station\\health_station.ctrl", out CachedTag healthCtrl))
+                            {
+                                var paletteIndex = (short)scnr.ControlPalette.FindIndex(e => e.Object == healthCtrl);
+
+                                // substitute with health pack equipment
+                                if (CacheContext.TagCache.TryGetCachedTag("objects\\powerups\\health_pack\\health_pack_large.eqip", out CachedTag healthEqip)) 
+                                {
+                                    scnr.EquipmentPalette.Add(new Scenario.ScenarioPaletteEntry()
+                                    {
+                                        Object = healthEqip
+                                    });
+
+                                    var eqipPaletteIndex = (short)(scnr.EquipmentPalette.Count - 1);
+
+                                    foreach (var controlInstance in scnr.Controls)
+                                    {
+                                        if (controlInstance.PaletteIndex == paletteIndex)
+                                        {
+                                            scnr.Equipment.Add(new Scenario.EquipmentInstance()
+                                            {
+                                                PaletteIndex = eqipPaletteIndex,
+                                                NameIndex = controlInstance.NameIndex,
+                                                PlacementFlags = controlInstance.PlacementFlags,
+                                                Position = controlInstance.Position,
+                                                Rotation = controlInstance.Rotation,
+                                                Scale = controlInstance.Scale,
+                                                NodeOrientations = controlInstance.NodeOrientations,
+                                                TransformFlags = controlInstance.TransformFlags,
+                                                ManualBspFlags = controlInstance.ManualBspFlags,
+                                                LightAirprobeName = controlInstance.LightAirprobeName,
+                                                UniqueHandle = controlInstance.UniqueHandle,
+                                                OriginBspIndex = controlInstance.OriginBspIndex,
+                                                ObjectType = controlInstance.ObjectType,
+                                                Source = controlInstance.Source,
+                                                BspPolicy = controlInstance.BspPolicy,
+                                                EditingBoundToBsp = controlInstance.EditingBoundToBsp,
+                                                EditorFolder = controlInstance.EditorFolder,
+                                                ParentId = controlInstance.ParentId,
+                                                CanAttachToBspFlags = controlInstance.CanAttachToBspFlags,
+                                                Multiplayer = controlInstance.Multiplayer,
+                                            });
+                                        }
+                                    }
+                                }
+
+                                if (paletteIndex != -1)
+                                    scnr.ControlPalette[paletteIndex].Object = null;
+
+                                RemoveNullPlacements(scnr.ControlPalette, scnr.Controls);
+                            }
                         }
 
                         ScenarioPort = true;
@@ -1433,101 +1480,6 @@ namespace TagTool.Commands.Porting
 
                     CacheContext.Serialize(cacheStream, forge_globals, forg);
                 }
-        }
-
-        private Effect ConvertEffect(Stream cacheStream, Effect effe, string blamTagName)
-        {
-            if (BlamCache.Platform == CachePlatform.MCC)
-            {
-                effe.Flags = effe.FlagsMCC.ConvertLexical<EffectFlags>();
-            }
-
-            if (BlamCache.Version >= CacheVersion.HaloReach)
-            {
-                effe.Priority = effe.PriorityReach;
-                effe.LocalLocation1 = effe.LocalLocation1Reach;
-            }
-
-            foreach (var effectEvent in effe.Events)
-            {
-                if (effectEvent.Parts.Any(p => p.Flags.HasFlag(EffectEventPartFlags.MakeEveryTick)) 
-                    && BlamCache.Platform == CachePlatform.MCC)
-                {
-                    effectEvent.DurationBounds.Lower *= 2;
-                    effectEvent.DurationBounds.Upper *= 2;
-                }
-
-                foreach (var particleSystem in effectEvent.ParticleSystems)
-                {
-                    if (BlamCache.Version < CacheVersion.Halo3ODST) //this value is inverted in ODST tags when compared to H3
-                    {
-                        particleSystem.NearRange = 1 / particleSystem.NearRange;
-
-                        if (particleSystem.Flags.HasFlag(Effect.Event.ParticleSystem.ParticleSystemFlags.OverrideNearFade))
-                            particleSystem.NearCutoff = particleSystem.NearFadeOverride;
-                    }
-
-                    if (BlamCache.Version >= CacheVersion.HaloReach)
-                    {
-                        Enum.TryParse(particleSystem.ReachFlags.ToString(), out particleSystem.Flags);
-
-                        for (int i = 0; i < particleSystem.Emitters.Count; i++)
-                        {
-                            var emitter = particleSystem.Emitters[i];
-                            // Needs to be implemented in the engine
-                            if (emitter.EmissionShape == Effect.Event.ParticleSystem.Emitter.EmissionShapeValue.BoatHullSurface ||
-                                emitter.EmissionShape == Effect.Event.ParticleSystem.Emitter.EmissionShapeValue.Jetwash)
-                            {
-                                new TagToolWarning($"Unsupported particle emitter shape '{emitter.EmissionShape}'. Using default.");
-                                emitter.EmissionShape = Effect.Event.ParticleSystem.Emitter.EmissionShapeValue.Sprayer;
-                            }
-
-                            if (emitter.AxisScale.X != 1.0f || emitter.AxisScale.Y != 1.0f || emitter.AxisScale.Z != 1.0f)
-                            {
-                                Effects.EmitterCustomPointPlotter pecpPlotter = new Effects.EmitterCustomPointPlotter(CacheContext, cacheStream, emitter, blamTagName, i);
-                                if (!pecpPlotter.ConvertEmitterToCustomPoints())
-                                    new TagToolWarning($"Particle emitter \"{CacheContext.StringTable.GetString(emitter.Name)}_{i}\" will have incorrect dimensions: AxisScale {emitter.AxisScale}");
-                            }
-
-                            if (!Enum.TryParse(emitter.ParticleMovement.FlagsReach.ToString(), out emitter.ParticleMovement.Flags))
-                                throw new FormatException(BlamCache.Version.ToString());
-                        }
-                    }
-
-                    // hack -- for some reason these emitters are killed when using gpu
-                    if (BlamCache.Version == CacheVersion.Halo3Retail &&
-                        (blamTagName == @"fx\cinematics\070la_waypoint_arrival\01\slipspace_rupture" ||
-                        blamTagName == @"fx\cinematics\070la_waypoint_arrival\01\slipspace_rupture_carrier" ||
-                        blamTagName == @"fx\cinematics\040lb_cov_flee\08\shot_1\slipspace_rupture" ||
-                        blamTagName == @"fx\cinematics\100lb_hc_crash\shot_4\slipspace_rupture"))
-                    {
-                        particleSystem.Emitters[0].EmitterFlags &= ~Effect.Event.ParticleSystem.Emitter.FlagsValue.IsGpu;
-                        particleSystem.Emitters[0].EmitterFlags |= Effect.Event.ParticleSystem.Emitter.FlagsValue.IsCpu;
-                    }
-                    if (BlamCache.Version == CacheVersion.Halo3ODST)
-                    {
-                        switch (blamTagName)
-                        {
-                            case @"fx\cinematics\c200\slipspace\slipspace_rupture" when CacheContext.StringTable.GetString(effectEvent.Name) == "rupture":
-                            case @"fx\cinematics\l200_out\slipspace\slipspace_rupture" when CacheContext.StringTable.GetString(effectEvent.Name) == "rupture":
-                                particleSystem.Emitters[0].EmitterFlags &= ~Effect.Event.ParticleSystem.Emitter.FlagsValue.IsGpu;
-                                particleSystem.Emitters[0].EmitterFlags |= Effect.Event.ParticleSystem.Emitter.FlagsValue.IsCpu;
-                                break;
-                        }
-                    }
-                    if (BlamCache.Platform == CachePlatform.MCC && BlamCache.Version >= CacheVersion.HaloReach)
-                    {
-                        foreach (var emitter in particleSystem.Emitters)
-                        {
-                            emitter.RuntimeMGpu.Properties = emitter.RuntimeMGpu.RuntimeGpuBlocks?[0].Properties;
-                            emitter.RuntimeMGpu.Functions = emitter.RuntimeMGpu.RuntimeGpuBlocks?[0].Functions;
-                            emitter.RuntimeMGpu.Colors = emitter.RuntimeMGpu.RuntimeGpuBlocks?[0].Colors;
-                        }
-                    }
-                }
-            }
-
-            return effe;
         }
 
         private static object ConvertDecoratorSetReach(DecoratorSet decoratorSet)
