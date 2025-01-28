f0fbfde56a0355823236df15b1f2c7877fdefda1
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2025-01-28 13:01:22 +1100

    Update Achievements Definition

--- TagTool/Tags/Definitions/Achievements.cs
+++ TagTool/Tags/Definitions/Achievements.cs
@@ -11,84 +11,172 @@ namespace TagTool.Tags.Definitions
     {
         public List<AchievementInformationBlock> AchievementInformation;
 
-        [TagStructure(Size = 0x18)]
+        [TagStructure(Size = 0x18, Platform = CachePlatform.Original)]
+        [TagStructure(Size = 0x10, Platform = CachePlatform.MCC)]
         public class AchievementInformationBlock : TagStructure
         {
-            public Achievements Achievement;
+            [TagField(Platform = CachePlatform.Original)]
+            public Achievements AchievementName;
+
+            [TagField(Platform = CachePlatform.MCC)]
+            public AchievementsMCC AchievementNameMCC;
+
             public LevelFlags Flags;
+
             [TagField(Flags = Label)]
-            public StringId LevelName;
-            public int Goal;
-            public ChudIconFlags IconFlags;
-            public int IconIndex;
+            public StringId RestrictedLevelName;
+
+            public int MaximumProgressCount;
+
+            [TagField(Platform = CachePlatform.Original)]
+            public int ProgressionTrackingInterval;
+
+            [TagField(Platform = CachePlatform.Original)]
+            public int ChudBitmapSequenceIndex;
 
             public enum Achievements : int
             {
-                beat_sc100,
-                beat_sc110,
-                beat_sc120,
-                beat_sc130,
-                beat_sc140,
-                beat_sc150,
-                beat_l200,
-                beat_l300,
-                beat_campaign_normal,
-                beat_campaign_heroic,
-                beat_campaign_legendary,
-                wraith_killer,
-                naughty_naughty,
-                good_samaritan,
-                dome_inspector,
-                laser_blaster,
-                both_tubes,
-                i_like_fire,
-                my_clothes,
-                pink_and_deadly,
-                dark_times,
-                trading_down,
-                headcase,
-                boom_headshot,
-                ewww_sticky,
-                junior_detective,
-                gumshoe,
-                super_sleuth,
-                metagame_points_in_sc100,
-                metagame_points_in_sc110,
-                metagame_points_in_sc120,
-                metagame_points_in_sc130a,
-                metagame_points_in_sc130b,
-                metagame_points_in_sc140,
-                metagame_points_in_l200,
-                metagame_points_in_l300,
-                be_like_marty,
-                find_all_audio_logs,
-                find_01_audio_logs,
-                find_03_audio_logs,
-                find_15_audio_logs,
-                vidmaster_challenge_deja_vu,
-                vidmaster_challenge_endure,
-                vidmaster_challenge_classic,
-                heal_up,
-                stunning,
-                tourist
+                BeatSc100,
+                BeatSc110,
+                BeatSc120,
+                BeatSc130,
+                BeatSc140,
+                BeatSc150,
+                BeatL200,
+                BeatL300,
+                BeatCampaignNormal,
+                BeatCampaignHeroic,
+                BeatCampaignLegendary,
+                WraithKiller,
+                NaughtyNaughty,
+                GoodSamaritan,
+                DomeInspector,
+                LaserBlaster,
+                BothTubes,
+                ILikeFire,
+                MyClothes,
+                PinkAndDeadly,
+                DarkTimes,
+                TradingDown,
+                Headcase,
+                BoomHeadshot,
+                EwwwSticky,
+                JuniorDetective,
+                Gumshoe,
+                SuperSleuth,
+                MetagamePointsInSc100,
+                MetagamePointsInSc110,
+                MetagamePointsInSc120,
+                MetagamePointsInSc130a,
+                MetagamePointsInSc130b,
+                MetagamePointsInSc140,
+                MetagamePointsInL200,
+                MetagamePointsInL300,
+                BeLikeMarty,
+                FindAllAudioLogs,
+                Find1AudioLog,
+                Find3AudioLogs,
+                Find15AudioLogs,
+                VidmasterChallengeDejaVu,
+                VidmasterChallengeEndure,
+                VidmasterChallengeClassic,
+                HealUp,
+                Stunning,
+                Tourist,
+            }
+
+            public enum AchievementsMCC : int
+            {
+                BeatSc100,
+                BeatSc110,
+                BeatSc120,
+                BeatSc130,
+                BeatSc140,
+                BeatSc150,
+                BeatL200,
+                BeatL300,
+                BeatCampaignNormal,
+                BeatCampaignHeroic,
+                BeatCampaignLegendary,
+                WraithKiller,
+                NaughtyNaughty,
+                GoodSamaritan,
+                DomeInspector,
+                LaserBlaster,
+                BothTubes,
+                ILikeFire,
+                MyClothes,
+                PinkAndDeadly,
+                DarkTimes,
+                TradingDown,
+                Headcase,
+                BoomHeadshot,
+                EwwwSticky,
+                JuniorDetective,
+                Gumshoe,
+                SuperSleuth,
+                MetagamePointsInSc100,
+                MetagamePointsInSc110,
+                MetagamePointsInSc120,
+                MetagamePointsInSc130a,
+                MetagamePointsInSc130b,
+                MetagamePointsInSc140,
+                MetagamePointsInL200,
+                MetagamePointsInL300,
+                BeLikeMarty,
+                FindAllAudioLogs,
+                Find1AudioLog,
+                Find3AudioLogs,
+                Find15AudioLogs,
+                VidmasterChallengeDejaVu,
+                VidmasterChallengeEndure,
+                VidmasterChallengeClassic,
+                HealUp,
+                Stunning,
+                Tourist,
+                OneGotAway,
+                MakerOfTheMadrigal,
+                Vandalized,
+                SaturdayMorningCartoon,
+                ForYourEyesOnly,
+                CatchEmNapping,
+                StrongSilentType,
+                BipBapBam,
+                PineappleExpress,
+                HarderThanItLooks,
+                MoreThanHisShare,
+                FowlHunt,
+                Xenophilla,
+                SpartanCan,
+                Firefly,
+                SaidTheLady,
+                SitDown,
+                Ghosthunter,
+                OneWayRide,
+                IAintDead,
+                DeferenceOfDarkness,
+                EyeForAnEye,
+                ThoseLeftBehind,
+                ShootAndScoot,
+                FastAndLow,
+                Shiny,
+                BitsAndPieces,
+                WhatAboutThoseTanks,
+                HogBeatsScorpion,
+                TwoPlacesSameTime,
+                Xenophobia,
+                KeepItClean,
+                VidmasterEndureFloodRedux,
             }
 
             [Flags]
             public enum LevelFlags : int
             {
                 None = 0,
-                InvalidInCampaign = 1 << 0,
-                InvalidInSurvival = 1 << 1,
-                ResetsOnMapReload = 1 << 2,
-                UsesGameProgression = 1 << 3,
-            }
-
-            [Flags]
-            public enum ChudIconFlags : int
-            {
-                None = 0,
-                DisplaysOnHud = 1 << 0,
-                Bit1 = 1 << 1 // only used for "super_sleuth"
+                DisabledInCampaign = 1 << 0,
+                DisabledInSurvival = 1 << 1,
+                DisabledInHub = 1 << 2,
+                DisabledInScreens = 1 << 3,
             }
         }
     }
