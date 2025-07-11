59fe2ca5b474eecdf48df84867f2b58232b3bc96
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2025-07-12 06:04:55 +1000

    Manually Add ReplaceTagCommand

---
+++ TagTool/Cache/Utils/TagReplacer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using TagTool.Cache.HaloOnline;
+using TagTool.Scripting;
+using TagTool.Tags;
+using TagTool.Tags.Definitions;
+
+namespace TagTool.Cache.Utils
+{
+    public static class TagReplacer
+    {
+        /// <summary>
+        /// Replaces all references to <paramref name="tag"/> with <paramref name="replacementTag"/>
+        /// </summary>
+        public static void ReplaceTag(GameCacheHaloOnlineBase cache, Stream cacheStream, CachedTag tag, CachedTag replacementTag)
+        {
+            CachedTag[] dependentTags = cache.TagCache
+                .NonNull()
+                .Cast<CachedTagHaloOnline>()
+                .Where(x => x.Dependencies.Contains(tag.Index))
+                .ToArray();
+
+            if (dependentTags.Length == 0)
+            {
+                Console.WriteLine($"No references to '{tag}' were found!");
+                return;
+            }
+
+            for (int i = 0; i < dependentTags.Length; i++)
+            {
+                CachedTag dependentTag = dependentTags[i];
+                Console.WriteLine($"{dependentTag}");
+
+                object definition = cache.Deserialize(cacheStream, dependentTag);
+                definition = ReplaceTagReferences(cache, definition, tag, replacementTag);
+                cache.Serialize(cacheStream, dependentTag, definition);
+
+                if (i < dependentTags.Length - 1)
+                    Console.WriteLine();
+            }
+        }
+
+        private static object ReplaceTagReferences(GameCacheHaloOnlineBase cache, object data, CachedTag tag, CachedTag replacementTag, string path = "")
+        {
+            switch (data)
+            {
+                case null:
+                case byte[] _:
+                    break;
+                case CachedTag tagRef:
+                    if (tagRef == tag)
+                    {
+                        Console.WriteLine($"  {path} = {replacementTag?.ToString() ?? "null"}");
+                        return replacementTag;
+                    }
+                    break;
+                case IList collection:
+                    for (int i = 0; i < collection.Count; i++)
+                    {
+                        collection[i] = ReplaceTagReferences(cache, collection[i], tag, replacementTag, $"{path}[{i}]");
+                    }
+                    break;
+                case TagStructure tagStruct:
+                    {
+                        foreach (var field in tagStruct.GetTagFieldEnumerable(cache.Version, cache.Platform))
+                        {
+                            string fieldPath = path == "" ? field.Name : $"{path}.{field.Name}";
+                            object fieldValue = field.GetValue(tagStruct);
+                            fieldValue = ReplaceTagReferences(cache, fieldValue, tag, replacementTag, fieldPath);
+                            field.SetValue(tagStruct, fieldValue);
+                        }
+
+                        if (tagStruct is Scenario scenario)
+                            data = ReplaceScriptTagReferences(scenario, tag, replacementTag);
+                    }
+                    break;
+
+            }
+            return data;
+        }
+
+        private static Scenario ReplaceScriptTagReferences(Scenario scenario, CachedTag tag, CachedTag replacementTag)
+        {
+            foreach (HsSyntaxNode node in scenario.ScriptExpressions)
+            {
+                if (node.Flags.HasFlag(HsSyntaxNodeFlags.Primitive)
+                    && node.ValueType.HaloOnline >= HsType.HaloOnlineValue.Sound
+                    && node.ValueType.HaloOnline <= HsType.HaloOnlineValue.AnyTagNotResolving)
+                {
+                    int srcTagIndex = BitConverter.ToInt32(node.Data, 0);
+                    if (srcTagIndex == tag.Index)
+                    {
+                        int exprIndex = scenario.ScriptExpressions.IndexOf(node);
+                        Console.WriteLine($"  ScriptExpressions[{exprIndex}].Data = {replacementTag?.ToString() ?? "null"}");
+                        node.Data = BitConverter.GetBytes(replacementTag?.Index ?? -1);
+                    }
+                }
+            }
+            return scenario;
+        }
+    }
+}
---
+++ TagTool/Commands/Tags/ReplaceTagCommand.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using TagTool.Cache;
+using TagTool.Cache.Utils;
+using TagTool.Commands.Common;
+
+namespace TagTool.Commands.Tags
+{
+    public class ReplaceTagCommand : Command
+    {
+        private readonly GameCacheHaloOnlineBase Cache;
+
+        public ReplaceTagCommand(GameCacheHaloOnlineBase cache)
+            : base(true,
+
+                "ReplaceTag",
+                "Replaces a tag with another tag from the cache",
+
+                "ReplaceTag <tag> <replacement tag>",
+
+                "")
+        {
+            Cache = cache;
+        }
+
+        public override object Execute(List<string> args)
+        {
+            if (args.Count < 2)
+                return new TagToolError(CommandError.ArgCount);
+
+            if (!Cache.TagCache.TryGetTag(args[0], out CachedTag tag))
+                return new TagToolError(CommandError.TagInvalid, args[0]);
+
+            if (!Cache.TagCache.TryGetTag(args[1], out CachedTag replacementTag))
+                return new TagToolError(CommandError.TagInvalid, args[1]);
+
+            using Stream cacheStream = Cache.OpenCacheReadWrite();
+
+            TagReplacer.ReplaceTag(Cache, cacheStream, tag, replacementTag);
+
+            return true;
+        }
+    }
+}
