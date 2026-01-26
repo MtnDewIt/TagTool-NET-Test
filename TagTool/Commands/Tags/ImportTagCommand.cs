fe88e93beac71ae59971e4d5108df259f2f67b19
Vegetal-Pole <vegetal.halo@gmail.com>
2026-01-26 17:22:14 -0600

    ImportTag compatibility check

--- TagTool/Commands/Tags/ImportTagCommand.cs
+++ TagTool/Commands/Tags/ImportTagCommand.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using TagTool.Cache;
-using TagTool.Commands.Common;
 using TagTool.Cache.HaloOnline;
-using TagTool.Extensions;
+using TagTool.Common;
+using TagTool.Common.Logging;
+using TagTool.Commands.Common;
 
 namespace TagTool.Commands.Tags
 {
@@ -44,6 +45,10 @@ namespace TagTool.Commands.Tags
                 inStream.ReadExactly(data);
             }
 
+            if (!FormatRecognized(in data, in instance))
+                return new TagToolError(CommandError.FileType, "Incompatible tag format."
+                    + "\nIf this tag is from an editing kit, try ImportLooseTag");
+
             using (var stream = Cache.OpenCacheReadWrite())
             {
                 Cache.TagCacheGenHO.SetTagDataRaw(stream, (CachedTagHaloOnline)instance, data);
@@ -54,6 +59,30 @@ namespace TagTool.Commands.Tags
             }
 
             Console.WriteLine($"Imported 0x{data.Length:X} bytes.");
+
+            return true;
+        }
+
+        private bool FormatRecognized(in byte[] data, in CachedTag instance)
+        {
+            if (data is null || data.Length < 0x30)
+                return false;
+
+            int size = BitConverter.ToInt32(data, 0x4);
+            if (size != data.Length)
+                return false;
+
+            Tag tag = new(BitConverter.ToInt32(data, 0x14));
+            if (!Cache.TagCache.TagDefinitions.TagDefinitionExists(tag))
+                return false;
+            if (instance.Group.Tag != tag)
+                Log.Warning($"Importing \'{tag}\' tag over instance of group \'{instance.Group.Tag}\'");
+
+            if (data.Length == 0x30)
+            {
+                Log.Warning($"Tag is empty (size = 0x30)");
+                return false;
+            }
 
             return true;
         }
