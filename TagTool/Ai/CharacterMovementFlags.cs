f855ecb00d31c6d2de1e92856b3b0daa3fd5f195
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2026-06-02 18:15:49 +1000

    Update Character Movement Flags

--- TagTool/Ai/CharacterMovementFlags.cs
+++ TagTool/Ai/CharacterMovementFlags.cs
@@ -1,103 +1,48 @@
-using System;
+using TagTool.Cache;
+using TagTool.Tags;
 
 namespace TagTool.Ai
 {
-    [Flags]
-    public enum CharacterMovementFlagsH3 : int
+    [TagEnum(IsVersioned = true)]
+    public enum CharacterMovementFlags
     {
-        None = 0,
-        DangerCrouchAllowMovement = 1 << 0,
-        NoSideStep = 1 << 1,
-        PreferToCombatNearFriends = 1 << 2,
-        HopToCoverPathSegements = 1 << 3,
-        HopToEndOfPath = 1 << 4,
-        AllowBoostedJump = 1 << 5,
-        Perch = 1 << 6,
-        Climb = 1 << 7,
-        PreferWallMovement = 1 << 8,
-        HasFlyingMode = 1 << 9,
-        DisallowCrouch = 1 << 10,
-        DisallowAllMovement = 1 << 11,
-        AlwaysUseSearchPoints = 1 << 12,
-        KeepMoving = 1 << 13,
-        CureIsolationJump = 1 << 14
-    }
-
-    [Flags]
-    public enum CharacterMovementFlagsODST : int
-    {
-        None = 0,
-        DangerCrouchAllowMovement = 1 << 0,
-        NoSideStep = 1 << 1,
-        PreferToCombatNearFriends = 1 << 2,
-        HopToCoverPathSegements = 1 << 3,
-        HopToEndOfPath = 1 << 4,
-        AllowBoostedJump = 1 << 5,
-        Perch = 1 << 6,
-        Climb = 1 << 7,
-        PreferWallMovement = 1 << 8,
-        HasFlyingMode = 1 << 9,
-        DisallowCrouch = 1 << 10,
-        DisallowAllMovement = 1 << 11,
-        AlwaysUseSearchPoints = 1 << 12,
-        KeepMoving = 1 << 13,
-        CureIsolationJump = 1 << 14,
-        OnlyUseAerialFiringPositions = 1 << 15,
-    }
-
-    [Flags]
-    public enum CharacterMovementFlagsReach : int
-    {
-        None = 0,
-        DangerCrouchAllowMovement = 1 << 0,
-        NoSideStep = 1 << 1,
-        PreferToCombatNearFriends = 1 << 2,
-        HopToCoverPathSegements = 1 << 3,
-        HopToEndOfPath = 1 << 4,
-        AllowBoostedJump = 1 << 5,
-        Perch = 1 << 6,
-        Climb = 1 << 7,
-        PreferWallMovement = 1 << 8,
-        HasFlyingMode = 1 << 9,
-        DisallowCrouch = 1 << 10,
-        DisallowAllMovement = 1 << 11,
-        AlwaysUseSearchPoints = 1 << 12,
-        KeepMoving = 1 << 13,
-        CureIsolationJump = 1 << 14,
-        GainElevation = 1 << 15,
-        RepositionDistant = 1 << 16,
-        OnlyUseAerialFiringPositions = 1 << 17,
-        UseHighPriorityPathFinding = 1 << 18,
-        LowerWeaponWhenNoAlertMovementOverride = 1 << 19
-    }
-
-    [Flags]
-    public enum CharacterMovementFlagsH4 : int
-    {
-        None = 0,
-        DangerCrouchAllowMovement = 1 << 0,
-        NoSideStep = 1 << 1,
-        PreferToCombatNearFriends = 1 << 2,
-        AllowBoostedJump = 1 << 3,
-        Perch = 1 << 4,
-        Climb = 1 << 5,
-        PreferWallMovement = 1 << 6,
-        HasFlyingMode = 1 << 7,
-        DisallowCrouch = 1 << 8,
-        DisallowAllMovement = 1 << 9,
-        AlwaysUseSearchPoints = 1 << 10,
-        KeepMoving = 1 << 11,
-        CureIsolationJump = 1 << 12,
-        GainElevation = 1 << 13,
-        RepositionDistant = 1 << 14,
-        OnlyUseAerialFiringPositions = 1 << 15,
-        UseHighPriorityPathFinding = 1 << 16,
-        LowerWeaponWhenNoAlertMovementOverride = 1 << 17,
-        Phase = 1 << 18,
-        NoOverrideWhenFiring = 1 << 19,
-        NoStowDuringIdleActivities = 1 << 20,
-        FlipAnyVehicle = 1 << 21,
+        DangerCrouchAllowMovement,
+        NoSideStep,
+        PreferToCombatNearFriends,
+        [TagEnumMember(MaxVersion = CacheVersion.HaloReach11883)]
+        HopToCoverPathSegements,
+        [TagEnumMember(MaxVersion = CacheVersion.HaloReach11883)]
+        HopToEndOfPath,
+        AllowBoostedJump,
+        Perch,
+        Climb,
+        PreferWallMovement,
+        HasFlyingMode,
+        DisallowCrouch,
+        DisallowAllMovement,
+        AlwaysUseSearchPoints,
+        KeepMoving,
+        CureIsolationJump,
+        [TagEnumMember(MinVersion = CacheVersion.HaloReach)]
+        GainElevation,
+        [TagEnumMember(MinVersion = CacheVersion.HaloReach)]
+        RepositionDistant,
+        [TagEnumMember(MinVersion = CacheVersion.Halo3ODST, MaxVersion = CacheVersion.HaloReach11883)]
+        OnlyUseAerialFiringPositions,
+        [TagEnumMember(MinVersion = CacheVersion.HaloReach)]
+        UseHighPriorityPathFinding,
+        [TagEnumMember(MinVersion = CacheVersion.HaloReach)]
+        LowerWeaponWhenNoAlertMovementOverride,
+        [TagEnumMember(MinVersion = CacheVersion.Halo4)]
+        Phase,
+        [TagEnumMember(MinVersion = CacheVersion.Halo4)]
+        NoOverrideWhenFiring,
+        [TagEnumMember(MinVersion = CacheVersion.Halo4)]
+        NoStowDuringIdleActivities,
+        [TagEnumMember(MinVersion = CacheVersion.Halo4)]
+        FlipAnyVehicle,
         // Not in H4 definition? Where's this from?
-        BoundAlongPath = 1 << 22
+        [TagEnumMember(MinVersion = CacheVersion.Halo4)]
+        BoundAlongPath,
     }
 }
