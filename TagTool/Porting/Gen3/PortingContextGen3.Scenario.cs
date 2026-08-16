cc0b046932f5076bfa23965f8def8ae5343abb1c
Minelelol <89744342+Minelelol@users.noreply.github.com>
2026-08-16 18:42:22 +0700

    Convert reach Kill Volume to using ED Kill Volume

--- TagTool/Porting/Gen3/PortingContextGen3.Scenario.cs
+++ TagTool/Porting/Gen3/PortingContextGen3.Scenario.cs
@@ -105,6 +105,67 @@ namespace TagTool.Porting.Gen3
 
                     RemoveNullPlacements(scnr.ControlPalette, scnr.Controls);
                 }
+                // use ED kill volume instead of reach
+                if (CacheContext.TagCache.TryGetCachedTag("objects\\multi\\boundaries\\kill_volume.scen", out CachedTag killVolumeScen))
+                {
+                    var paletteIndices = new List<short>();
+
+                    for (short i = 0; i < scnr.SceneryPalette.Count; i++)
+                        if (scnr.SceneryPalette[i].Object == killVolumeScen)
+                            paletteIndices.Add(i);
+
+                    if (paletteIndices.Count > 0 &&
+                        CacheContext.TagCache.TryGetCachedTag("objects\\multi\\boundaries\\kill_volume.bloc", out CachedTag killVolumeCrate))
+                    {
+                        scnr.CratePalette.Add(new Scenario.ScenarioPaletteEntry()
+                        {
+                            Object = killVolumeCrate
+                        });
+
+                        var cratePaletteIndex = (short)(scnr.CratePalette.Count - 1);
+
+                        foreach (var sceneryInstance in scnr.Scenery)
+                        {
+                            if (paletteIndices.Contains(sceneryInstance.PaletteIndex))
+                            {
+                                scnr.Crates.Add(new Scenario.CrateInstance()
+                                {
+                                    PaletteIndex = cratePaletteIndex,
+                                    NameIndex = sceneryInstance.NameIndex,
+                                    PlacementFlags = sceneryInstance.PlacementFlags,
+                                    Position = sceneryInstance.Position,
+                                    Rotation = sceneryInstance.Rotation,
+                                    Scale = sceneryInstance.Scale,
+                                    NodeOrientations = sceneryInstance.NodeOrientations,
+                                    TransformFlags = sceneryInstance.TransformFlags,
+                                    ManualBspFlags = sceneryInstance.ManualBspFlags,
+                                    LightAirprobeName = sceneryInstance.LightAirprobeName,
+                                    ObjectId = new ObjectIdentifier
+                                    {
+                                        UniqueId = sceneryInstance.ObjectId.UniqueId,
+                                        OriginBspIndex = sceneryInstance.ObjectId.OriginBspIndex,
+                                        Type = new GameObjectType8() { Halo3ODST = GameObjectTypeHalo3ODST.Crate },
+                                        Source = sceneryInstance.ObjectId.Source,
+                                    },
+                                    BspPolicy = sceneryInstance.BspPolicy,
+                                    EditingBoundToBsp = sceneryInstance.EditingBoundToBsp,
+                                    EditorFolder = sceneryInstance.EditorFolder,
+                                    ParentId = sceneryInstance.ParentId,
+                                    CanAttachToBspFlags = sceneryInstance.CanAttachToBspFlags,
+                                    Multiplayer = sceneryInstance.Multiplayer,
+                                });
+
+                                if (sceneryInstance.NameIndex != -1)
+                                    scnr.ObjectNames[sceneryInstance.NameIndex].ObjectType = new GameObjectType16() { Halo3ODST = GameObjectTypeHalo3ODST.Crate };
+                            }
+                        }
+
+                        foreach (var idx in paletteIndices)
+                            scnr.SceneryPalette[idx].Object = null;
+
+                        RemoveNullPlacements(scnr.SceneryPalette, scnr.Scenery);
+                    }
+                }
             }
 
             foreach (var zoneset in scnr.ZoneSets)
